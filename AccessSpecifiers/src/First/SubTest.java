595b7602de1774093494c489025818a79240a3de
lappy <neerukondavakula@gmail.com>
2017-06-06 11:25:50 -0700

    delete

--- AccessSpecifiers/src/First/SubTest.java
+++
@@ -1,9 +0,0 @@
-package First;
-
-public class SubTest extends Test {
-	public static void main(String[] args){
-		SubTest x=new SubTest();
-		System.out.println(x.getNum());
-	}
-
-}
--- AccessSpecifiers/src/Second/Test1.java
+++
@@ -1,9 +0,0 @@
-package Second;
-import First.*;
-public class Test1 {
-
-	public static void main(String[] args){
-		Test1 t2=new Test1();
-		//System.out.println(t2.getNum());
-	}
-}
