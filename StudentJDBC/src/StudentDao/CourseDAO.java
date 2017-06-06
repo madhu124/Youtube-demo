8264fe0ece6212c431dae4bf3e7ef9a9b173ddbe
lappy <neerukondavakula@gmail.com>
2017-06-06 10:40:53 -0700

    remove file

--- StudentJDBC/src/StudentDao/CourseDAO.java
+++
@@ -1,8 +0,0 @@
-package StudentDao;
-
-public interface CourseDAO {
-	public void addCourse();
-	public void editCourse();
-	public void deleteCourse();
-	public void viewCourse();
-}
--- StudentJDBC/src/StudentDao/StudentDAO.java
+++
@@ -1,9 +0,0 @@
-package StudentDao;
-
-public interface StudentDAO {
-public void addStudent();
-public void editStudent();
-public void deleteStudent();
-public void viewStudent();
-
-}
